2af240044091cdb4074e4070d41983055889baba
Mohammed Noori <kmanoorib@gmail.com>
2018-12-18 22:41:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    # i used line 10 in reversed which can get the job done alone, without line 11 and 12
+    # <%= params[:string].reverse %>
     original_string = params["string"]
     reversed_string = original_string.reverse
     
